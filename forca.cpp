99aa141ac57fb5fd5961f5d42c3415d3dc77fcf0
Anna Moraco <annamoraco@gmail.com>
2021-04-26 08:55:03 -0300

    Adicionando verificação do chute

--- forca.cpp
+++ forca.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 #include <string>
+#include <clocale>
 
 using namespace std;
 
+const string PALAVRA_SECRETA = "ABACAXI";
+string palavra_auxiliar;
+
+bool letraexiste(char chute){
+    for (char letra : PALAVRA_SECRETA){
+        if (chute == letra){
+            return true;
+        }
+    }
+    return false;
+}
+
 int main(){
 
+    setlocale(LC_ALL, "");
     cout << "BEM VINDO AO JOGO DA FORCA!" << endl;
     cout << "TENTE ADIVINHAR A PALAVRA " << endl << endl;
-
-    string palavra_secreta = "ABACAXI";
-    // cout << palavra_secreta << endl;
 
     bool enforcou = false;
     bool acertou = false;
@@ -20,7 +31,11 @@ int main(){
         cout << "Chute uma letra: ";
         cin >> chute;
         
-        cout << "Seu chute foi: " << chute << endl << endl; 
+        if (letraexiste(chute)){
+            cout << "Acertou a letra!" << endl << endl;
+        } else {
+            cout << "A palavra não tem essa letra." << endl << endl;
+        }
           
     }
 }
